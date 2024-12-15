2dbd3bb09c12a4fb052f2056e44d151af0dd17e5
daniel.baumann <baumann-dan@outlook.com>
2024-12-15 09:50:59 +0100

    add unit test for dns_header_to_buffer function

--- lib/test/compass_dns_test.c
+++ lib/test/compass_dns_test.c
@@ -27,9 +27,41 @@ void parse_dns_header__successfully() {
     TEST_ASSERT_EQUAL(4, dns_header.ar_count);
 }
 
-// not needed when using generate_test_runner.rb
+void dns_header_to_buffer__successfully() {
+    const DnsHeader dns_header = {
+        .id = 257,
+        .qr = 1,
+        .opcode = STATUS,
+        .aa = 1,
+        .tc = 1,
+        .rd = 1,
+        .ra = 1,
+        .z = 1,
+        .rcode = REFUSED,
+        .qd_count = 4,
+        .an_count = 3,
+        .ns_count = 2,
+        .ar_count = 1
+    };
+    u_int8_t buffer[12] = {0};
+    dns_header_to_buffer(&dns_header, buffer);
+    TEST_ASSERT_EQUAL(0x01, buffer[0]);
+    TEST_ASSERT_EQUAL(0x01, buffer[1]);
+    TEST_ASSERT_EQUAL(0x97, buffer[2]);
+    TEST_ASSERT_EQUAL(0x95, buffer[3]);
+    TEST_ASSERT_EQUAL(0x00, buffer[4]);
+    TEST_ASSERT_EQUAL(0x04, buffer[5]);
+    TEST_ASSERT_EQUAL(0x00, buffer[6]);
+    TEST_ASSERT_EQUAL(0x03, buffer[7]);
+    TEST_ASSERT_EQUAL(0x00, buffer[8]);
+    TEST_ASSERT_EQUAL(0x02, buffer[9]);
+    TEST_ASSERT_EQUAL(0x00, buffer[10]);
+    TEST_ASSERT_EQUAL(0x01, buffer[11]);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(parse_dns_header__successfully);
+    RUN_TEST(dns_header_to_buffer__successfully);
     return UNITY_END();
 }
