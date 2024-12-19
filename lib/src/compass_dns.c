38e9eb6a61e38e4b15e8574617a8d611d84b30fc
daniel.baumann <baumann-dan@outlook.com>
2024-12-19 19:59:32 +0100

    add short_to_big_endian_chars()

--- lib/src/compass_dns.c
+++ lib/src/compass_dns.c
@@ -1,12 +1,16 @@
 #include <string.h>
-#include <arpa/inet.h>
 
 #include "compass_dns.h"
 
 #define STRING_END '\0'
 
-static u_int16_t big_endian_chars_to_short(const uint8_t most_sig_char, const uint8_t least_sig_char) {
+static u_int16_t big_endian_chars_to_short(const u_int8_t most_sig_char, const u_int8_t least_sig_char) {
     return most_sig_char * 256 + least_sig_char;
+}
+
+static void short_to_big_endian_chars(u_int8_t *big_endian_chars_ptr, const u_int16_t value) {
+    big_endian_chars_ptr[1] = value % 256;
+    big_endian_chars_ptr[0] = (value - big_endian_chars_ptr[1]) / 256;
 }
 
 void parse_dns_header(const u_int8_t *buffer_ptr, DnsHeader *dns_header_ptr) {
@@ -26,8 +30,7 @@ void parse_dns_header(const u_int8_t *buffer_ptr, DnsHeader *dns_header_ptr) {
 }
 
 void dns_header_to_buffer(const DnsHeader *dns_header_ptr, u_int8_t *buffer_ptr) {
-    const u_int16_t id = htons(dns_header_ptr->id);
-    memcpy(buffer_ptr, &id, 2);
+    short_to_big_endian_chars(buffer_ptr, dns_header_ptr->id);
     buffer_ptr[2] = 0;
     if (dns_header_ptr->qr) buffer_ptr[2] += QR_BYTE_MASK;
     buffer_ptr[2] += dns_header_ptr->opcode << 3;
@@ -38,14 +41,10 @@ void dns_header_to_buffer(const DnsHeader *dns_header_ptr, u_int8_t *buffer_ptr)
     if (dns_header_ptr->ra) buffer_ptr[3] += RA_BYTE_MASK;
     buffer_ptr[3] += dns_header_ptr->z << 4;
     buffer_ptr[3] += dns_header_ptr->rcode;
-    const u_int16_t qd_count = htons(dns_header_ptr->qd_count);
-    memcpy(buffer_ptr + 4, &qd_count, 2);
-    const u_int16_t an_count = htons(dns_header_ptr->an_count);
-    memcpy(buffer_ptr + 6, &an_count, 2);
-    const u_int16_t ns_count = htons(dns_header_ptr->ns_count);
-    memcpy(buffer_ptr + 8, &ns_count, 2);
-    const u_int16_t ar_count = htons(dns_header_ptr->ar_count);
-    memcpy(buffer_ptr + 10, &ar_count, 2);
+    short_to_big_endian_chars(buffer_ptr + 4, dns_header_ptr->qd_count);
+    short_to_big_endian_chars(buffer_ptr + 6, dns_header_ptr->an_count);
+    short_to_big_endian_chars(buffer_ptr + 8, dns_header_ptr->ns_count);
+    short_to_big_endian_chars(buffer_ptr + 10, dns_header_ptr->ar_count);
 }
 
 void parse_dns_questions(
@@ -86,11 +85,9 @@ void parse_dns_questions(
         }
         if (domain_pointer_end_index > 0) buffer_index = domain_pointer_end_index;
         dns_questions_ptr->domain[domain_index] = STRING_END;
-        memcpy(&dns_questions_ptr->q_type, buffer_ptr + buffer_index, 2);
-        dns_questions_ptr->q_type = ntohs(dns_questions_ptr->q_type);
+        dns_questions_ptr->q_type = big_endian_chars_to_short(buffer_ptr[buffer_index], buffer_ptr[buffer_index + 1]);
         buffer_index += 2;
-        memcpy(&dns_questions_ptr->q_class, buffer_ptr + buffer_index, 2);
-        dns_questions_ptr->q_class = ntohs(dns_questions_ptr->q_class);
+        dns_questions_ptr->q_class = big_endian_chars_to_short(buffer_ptr[buffer_index], buffer_ptr[buffer_index + 1]);
         buffer_index += 2;
     }
     *questions_buffer_end_index_ptr = buffer_index - 1;
